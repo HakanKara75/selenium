6676eb53240ddd229d2a1eeaaa303d224f7e7d8c
HakanKara75 <hakanbatirhan@gmail.com>
2023-03-26 16:12:41 +0300

    v

--- src/test/java/day04_XpathCssSelector/C02_CssSelector.java
+++ src/test/java/day04_XpathCssSelector/C02_CssSelector.java
@@ -9,31 +9,38 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class C02_CssSelector {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("chromeDriver", "src/source/driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
 
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         //https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-       // 2- Add Element butonuna basin
-        WebElement sec= driver.findElement(By.cssSelector("input[onclick='addElement()']"));
+        Thread.sleep(3000);
+        // 2- Add Element butonuna basin
+        //tag_name[@attributeName='atributeDegeri']
+
+//xpath== >   //WebElement sec=driver.findElement(By.xpath("//*[@id='content']/div/button")).click();
+//css== >    //WebElement sec=driver.findElement(By.cssSelector("button[onclick='addElement()']"));
+
+        WebElement sec = driver.findElement(By.xpath("//button[text()='Add Element']"));
         sec.click();
+        //Delete butonu’nun gorunur oldugunu test edin
+        System.out.println("Is delete button displayed:" +sec.isDisplayed());
+
+        //Delete tusuna basin
+        WebElement del = sec.findElement(By.xpath("//*[@id='elements']/button"));
+        //WebElement del =driver.findElement(By.cssSelector("button[class='added-manually']"));
+        del.click();
+        Thread.sleep(2000);
+
+
+        //Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        System.out.println("Is add/remove button displayed:" +sec.isDisplayed());
+
         driver.close();
-        /*
-Delete butonu’nun gorunur oldugunu test edin
-Delete tusuna basin
-“Add/Remove Elements” yazisinin gorunur oldugunu test edin
-         */
-
-
-
-
-
-
-
 
     }
 }
